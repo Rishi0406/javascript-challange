f75557528d444fee23b69c7a6e17db7e5a5937d7
Rishi0406 <63531215+Rishi0406@users.noreply.github.com>
2020-09-06 04:26:57 +1000

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,21 +1,23 @@
 // from data.js
 var tableData = data;
 
+//select table body and filter button
 var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
+
+//create event handler
 button.on("click", filterTable);
 
+defaultLoad()   //show all sightings
+
+//grabs input value to filter table data
 function filterTable() {
-    d3.event.preventDefault();
-    var inputValue = d3.select("#datetime").property("value");
+    d3.event.preventDefault();   //prevent page from refreshing
+    var inputValue = d3.select("#datetime").property("value"); //get date to filter
+    var dataResult = tableData.filter(ufo => ufo.datetime === inputValue);  //apply filter
+    tbody.html("");                             
 
-    if(inputValue === '') {
-        defaultLoad();
-    }
-    else {
-        var dataResult = tableData.filter(ufo => ufo.datetime === inputValue);
-        tbody.html("");
-
+//show sightings by filtered date
         dataResult.forEach((ufoSighted) => {
             var row = tbody.append("tr");
             Object.entries(ufoSighted).forEach(([key, value]) => {
@@ -23,9 +25,9 @@ function filterTable() {
             cell.text(value);
             });
         });
-    }
-};
+    };
 
+//function to show all sightings from table
 function defaultLoad() {
     tableData.forEach((ufoSighted) => {
         var row = tbody.append("tr");
