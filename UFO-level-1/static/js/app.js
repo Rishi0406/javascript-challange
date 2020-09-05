5cf0d89485fc1d900f9990149e15ca6b0b0a9c30
Rishi0406 <63531215+Rishi0406@users.noreply.github.com>
2020-09-06 03:40:42 +1000

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,27 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+button.on("click", filterTable);
+
+function filterTable() {
+    d3.event.preventDefault();
+    var inputValue = d3.select("#datetime").property("value");
+
+    if(inputValue === '') {
+        defaultLoad();
+    }
+    else {
+        var dataResult = tableData.filter(ufo => ufo.datetime === inputValue);
+        tbody.html("");
+
+        dataResult.forEach((ufoSighted) => {
+            var row = tbody.append("tr");
+            Object.entries(ufoSighted).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+            });
+        });
+    }
+};
